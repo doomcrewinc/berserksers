2b6afd1e400acdecb19552d6d31deaea24c5c466
drewsauce <druppel@goldencomm.com>
2018-03-06 14:32:19 -0800

    restructure resources

---
+++ app/Classes/Whois.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drupp
+ * Date: 3/6/2018
+ * Time: 1:38 PM
+ */
+
+namespace App\Classes;
+
+
+class Whois
+{
+
+}
---
+++ app/Controllers/WhoisController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drupp
+ * Date: 3/6/2018
+ * Time: 1:39 PM
+ */
+
+namespace App\Controllers;
+
+
+class WhoisController
+{
+
+}
